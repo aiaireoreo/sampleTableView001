00b8f3aeaecc581e71a2318d98ef2f04f27ea6b6
AI Matsubara <aiai.1116.info@gmail.com>
2016-05-09 10:42:50 +0800

    initial commit

--- sampleTableView001/ViewController.swift
+++ sampleTableView001/ViewController.swift
@@ -8,12 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
 
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+    //表示する行数の決定
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10 //10行表示
+    }
+    
+    //行に表示する内容をセット（表示するセルの中身）
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = UITableViewCell(style: .Default, reuseIdentifier: "myCell")
+        
+        cell.textLabel!.text = "\(indexPath.row)行目"
+        //セルの中に時を表示する
+        //index.Pathは今、何行目のデータを出そうとしているかの行データの情報が詰まってる
+        //.rowは行番号
+        //
+        return cell
+        
+        }
+    
+    
+    //行を選択された時に行う処理
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath:NSIndexPath){
+        print("\(indexPath.row)行目を選択")
+    }
+        
+        //ステータスバーを非表示にする
+    override func prefersStatusBarHidden() -> Bool {
+            return true
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
